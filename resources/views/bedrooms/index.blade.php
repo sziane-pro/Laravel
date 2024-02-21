24cfc70742f2f8fe6c79967099bcd55a798917a4
Samy ZIANE <samy.ziane-pro@outlook.com>
2024-02-21 09:02:07 +0100

    updateé

--- resources/views/bedrooms/index.blade.php
+++ resources/views/bedrooms/index.blade.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../resources/css/hotels-index.css">
     <title>{{$hotel->nom}} | Chambres</title>
 </head>
 
 <body>
+    <x-header/>
     <a href="{{ route('bedrooms.create', $hotel->id)}}">Ajouter une chambre</a>
     <table>
         <thead>
